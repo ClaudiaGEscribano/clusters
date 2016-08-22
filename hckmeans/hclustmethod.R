423a52c77791929d0babeba3075965c660e990b0
Claudia <claudia.g.escribano@gmail.com>
2016-08-22 15:37:01 +0200

    Añado un tercer script

---
+++ hckmeans/hclustmethod.R
@@ -0,0 +1,52 @@
+library(raster)
+library(rasterVis)
+library(maps)
+library(maptools)
+library(rgdal)
+library(mapdata)
+library(zoo)
+library(ncdf4)
+
+#################################################################
+## 1. LOAD THE DATA
+###############################################################
+
+load("datahcmaskPCA.Rdata")
+
+#############################################################
+## 3. Hierarquical clustering
+#############################################################
+
+## I applied a hierarquical criterion in order to inizilitates the afterwards kmeans clustering.
+
+dataMatrix <- as.matrix(datahcmask)
+dataDist <- dist(dataMatrix) ## computes the distance matrix in order to use it in the hc algorithm
+
+datahclus <- hclust(dataDist)
+
+## I get the cut the clustering tree for k=2:70 clusters. I will decide the optimun number in next steps.
+
+kut <- cutree(datahclus, k=2:70)
+
+## I have to select the centroids of the clusters in order to use them in the kmeans clustering.
+ 
+
+cluster.centroid <- function(datahc, data, n){ ## n es el numero de clusters max. menos uno.
+     lapply(seq(from=1, to=n),
+            FUN=function(x) lapply(seq(from=1, to=x+1),
+                FUN=function(i){
+                    ind  <- which(datahc[,x]==i) ## cambio aqui  i por x
+                    r <- data[ind,]
+                    c <- colMeans(r)
+                    return(c)
+                })
+            )
+ }
+
+
+
+centroids <- cluster.centroid(kut, dataMatrix, 69) ## contiene una lista de 70 elementos. Cada uno de los elementos es otra lista con los valores de de los centroides, que son vectores de dimensión d igual a las columnas de data.
+
+## saving centroids, just if you need to check
+
+save(centroids, file='centroidshc.Rdata')
